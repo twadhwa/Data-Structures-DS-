78c61bf846f95e1e840e40e5f99f01b46b0e13c4
Tushar Wadhwa <66528495+twadhwa@users.noreply.github.com>
2021-01-13 23:53:31 +0530

    Add files via upload
    Linked list is a linear data structure where each node has the address of the next node. In this tutorial we have a basic structure of linked list with some operation on it .

--- main.cpp
+++ main.cpp
@@ -1,113 +1,61 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
+// Linked list
+
+#include<stdlib.h>
+#include <stdio.h >
 
 
-using namespace std;
+// Data type to represent a particular node in a linked list
+struct node {
 
-// Function to generate a random number
+    // Variable to store the data of the node
+    int data ;
 
-int random_number_generator() {
-    srand((signed)  time (0));
-    int rand_number = rand()  ;
+    // struct node type pointer to store the address of the next node
+    struct node * next ;
 
-
-    return rand_number ;
 }
+// This pointer is same a struct node type pointer that will store the address
+// of the first node
+
+// It is a global pointer that means it can be used directly in the every function
+
+*first = NULL;
 
 
-// Function to get input from the user and to store them in an array
-int input_getter(int number_of_players, int level){
+// Function to create an linked using an array
+void create (int arr[], int size_of_array){
 
-    // Variable to store the number that will be used in our game
-    int random_number ;
+    // A temporary pointer  to create a new node each time a number is entered
+    // and a pointer that will point on the last node every time so that we can add
+    // a new node every time at the last
+    struct node * new_node , * last ;
 
-    // Variable to store the user input
-    int user_input ;
+    // Now we will create a new node in the heap using malloc function
 
-    // Array to store the scores of every player
-    int score_array[number_of_players];
-
-    // Loop that will run 1 time for each player
-    for (int i =0 ; i < number_of_players  ; i ++ ){
-
-
-        // Statements for generating the number depending upon the level enter by the user
-        if (level == 1 ){
-
-            // %  will generate a number less than the given number
-            random_number = random_number_generator() % 20 ;
-        }
-
-        if (level == 2 ){
-            random_number = random_number_generator() % 50 ;
-        }
-
-        if (level == 3){
-            random_number = random_number_generator() % 100 ;
-
-        }
-
-        cout<<"PLAYER " << i+1  << " will play now "<<endl;
-
-
-        // Variable to store the number of turns taken by the player
-        int  no_of_guesses = 0  ;
-
-
-        // Loop for taking input from the user and to compare it with the generated number
-        do {
-            cout<<"Enter the number you want"<<endl;
-            cin>> user_input;
-            if (user_input > random_number ){
-                cout<<"Please enter a smaller number"<<endl;
+    // Pointer first will point on that node type of the structure
+    first = (struct node * ) malloc (sizeof(struct node ));
 
 
 
-            }
+    first -> data = arr[0];
+    first -> next = NULL;
 
-            if (user_input < random_number ){
-                cout<<"Please enter a larger number"<<endl;
+    // So there's only one element in the linked list, therefore the first is equal to last
+    last = first ;
 
+    // Now we will make for loop that will assign the values of the array to linked list
 
+    // We are starting  from one because we have already assigned first value
+    for (int i = 1 ; i < size_of_array ; i ++ ){
 
-            }
-
-            
-
-            no_of_guesses++;
-
-
-
-
-        }
-        while (user_input != random_number);
-        score_array[i] = no_of_guesses;
-
-
+        new_node = (struct node *) malloc (sizeof(struct node ));
+        new_node -> data = arr[i];
+        new_node->next = NULL;
+        last -> next = new_node ;
+        last = new_node ;
 
 
     }
-
-
-    // Variable to store the least turns
-    int least_turns = score_array[0];
-
-    // Variable to store the position of the winner player
-    int winner = 1 ;
-    for (int i =0 ; i < number_of_players ; i ++ ){
-        if (score_array[i] < least_turns){
-            least_turns = score_array[i];
-            winner = i ;
-        }
-
-        cout << "The number of turns of every player "<< i + 1   << " is "<< score_array[i]<< endl;
-
-    }
-
-    cout<<"The winner of the game is player " << winner << " with turns "<< least_turns<< endl;
-
-    return 0 ;
 
 
 
@@ -115,26 +63,82 @@ int input_getter(int number_of_players, int level){
 
 }
 
+void display (struct node * p){
+    // Here the input will be the pointer pointing on the first node
+    // As first pointer is a global variable it can be directly accessed but we are using another pointer for simplification
+    printf("The numbers of the linked list are \n");
+    while (p!= NULL) {
 
+        printf("%d", p->data);
+        printf(" ");
+        p = p->next;
+    }
 
+}
 
+// Function to count number of elements in a linked list
+int count_elements (struct node * p ){
 
+    // Variable to store the count of number of elements
+    int count = 0  ;
+    while (p!=NULL){
+        count ++;
+        p=p->next ;
 
-int main () {
+    }
 
-    int number_of_players ;
-    int level ;
-    cout<<"Enter the number of players that will play the game "<<endl ;
-    cin >> number_of_players;
-    cout<<"Enter the level of the game you want to play on "<< endl;
-    cin>> level ;
-
-    input_getter(number_of_players, level);
+    printf("The number of elements in the linked list are %d " , count );
     return 0 ;
 
+}
+
+// Function to give the sum of the elements of the linked list
+int sum_linked_list (struct node * p ){
+
+    // Variable to store the sum of the linked list
+    int sum =0 ;
+    while (p!= NULL){
+        sum = sum + p ->data ;
+        p= p->next ;
+    }
+    printf("The sum of the linked list is %d " , sum );
+    return 0 ;
+}
+
+int max_in_linked_list(struct node * p ){
+
+    // Variable to store the max element of the linked list
+    int max = p -> data;
+    while (p!=NULL){
+        if (p->data > max ){
+            max =p ->data;
+
+        }
+        p=p->next ;
+
+    }
+    printf("The max element in the linked list is %d" , max );
+    return 0 ;
+
+}
+
+// Function to search element in a linked list
+// In a linked list we do not use binary search because we cannot directly access the middle element of the linked list
+int searching (struct node * p , int key ){
 
 
+}
 
+
+int main (){
+
+    // An array that will be converted to an linked list
+    int arr[] = {1,2,6,4 ,7};
+    create(arr, 5 );
+    display (first );
+    printf("\n");
+    max_in_linked_list(first);
+    return 0 ;
 
 
 }
